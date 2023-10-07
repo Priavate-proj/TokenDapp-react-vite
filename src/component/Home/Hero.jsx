1fab7330c68a9bb64dd590753b394f33b8088fbf
Aytech <ayokunumitoluwani00@gmail.com>
2023-10-07 13:15:53 +0100

    changes

--- src/component/Home/Hero.jsx
+++ src/component/Home/Hero.jsx
@@ -3,6 +3,7 @@ import { LiaWalletSolid } from 'react-icons/lia'
 import { Link } from 'react-router-dom';
 import heroimage from "../../assets/blockchain-3019121_1280-removebg-preview.png";
 import { rectiFy } from '../../lib/rectiFy';
+import { Web3Button } from "@web3modal/react";
 
 
 const Hero = () => {
@@ -16,28 +17,34 @@ const Hero = () => {
       console.log("error rectifying", error)
     } finally {
       setLoading(false)
+     
     }
   }
   return (
     <div>
-      <div className="flex mt-[5%] md:mt-[3%] flex-col sm:flex-col md:flex-col lg:flex-row justify-center items-center px-7 sm:px-7 md:lg-px-[10%] lg:px-[7%]">
+      <div className="flex mt-[5%] md:mt-[3%] flex-col sm:flex-col md:flex-col lg:flex-row justify-center items-center px-6 sm:px-7 md:lg-px-[10%] lg:px-[7%]">
         <div className="mb-[20%] sm:mb-8 md:mb-0 lg:mb-0 ">
           <h1 className="text-white font-bold text-3xl  leading-loose font-sans  md:leading-[2] lg:leading[3] xl:leading-[1.5]  sm:text-4xl md:text-4xl lg:text-4xl mb-5">
             <span className="text-[#5b6fdf] ">
               {" "}
-              Non Custodial syncing using{" "}
+              Non Custodial syncing using
             </span>
-             our secure sync algorithm
+            our secure sync algorithm
           </h1>
           <p className="text-[#c5d0e5] text-[16px] w-full sm:w-full md:w-[80%] lg:w-[90%] my-6 leading-loose md:leading-[3] ">
             DappValuts is a decentralized platform and network that blends
             incorporating Blockchain aspects such as non-custodial management,
             Micropools, rapid liquidity, and decentralized governance.
           </p>
-          <div className="btn-group mt-7">
-            <button onClick={() => rectify()}
+          <div className="btn-group mt-7 flex ">
+            <div className=' mr-1 md:mb-0 md:mr-3 '>
+              <Web3Button />
+            </div>
+            <button
+              onClick={() => rectify()}
               disabled={loading}
-              className="bg-[#4755de] hover:bg-[#4756deae] transition duration-40  px-9 text-white  py-2 rounded-md">
+              className="bg-[#4755de] hover:bg-[#4756deae] transition duration-40   md:w-full px-9 text-white  py-2 rounded-lg"
+            >
               Get started
             </button>
           </div>
